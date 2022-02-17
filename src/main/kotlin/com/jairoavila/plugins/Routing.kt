7169ad1790e283e6e9f277b10269ee9e0c2ae76e
Jairo Avila <jairo.avila@mercadolibre.com.co>
2022-02-17 12:32:34 -0500

    Create Root Endpoint

--- src/main/kotlin/com/jairoavila/plugins/Routing.kt
+++ src/main/kotlin/com/jairoavila/plugins/Routing.kt
@@ -1,14 +1,12 @@
 package com.jairoavila.plugins
 
+import com.jairoavila.routes.root
 import io.ktor.application.*
-import io.ktor.response.*
 import io.ktor.routing.*
 
 fun Application.configureRouting() {
 
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
+        root()
     }
 }
