f4152ce24fcf0de7acf4392352d7263b6e37e8ff
Jairo Avila <jairo.avila@mercadolibre.com.co>
2022-02-17 12:27:07 -0500

    Create HeroRepository

---
+++ src/main/kotlin/com/jairoavila/repository/HeroRepositoryImpl.kt
@@ -0,0 +1,28 @@
+package com.jairoavila.repository
+
+import com.jairoavila.models.ApiResponse
+import com.jairoavila.models.Hero
+
+class HeroRepositoryImpl : HeroRepository {
+
+    override val heroes: Map<Int, List<Hero>>
+        get() = TODO("Not yet implemented")
+    override val page1: List<Hero>
+        get() = TODO("Not yet implemented")
+    override val page2: List<Hero>
+        get() = TODO("Not yet implemented")
+    override val page3: List<Hero>
+        get() = TODO("Not yet implemented")
+    override val page4: List<Hero>
+        get() = TODO("Not yet implemented")
+    override val page5: List<Hero>
+        get() = TODO("Not yet implemented")
+
+    override suspend fun getAllHeroes(page: Int): ApiResponse {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun searchHeroes(name: String): ApiResponse {
+        TODO("Not yet implemented")
+    }
+}
