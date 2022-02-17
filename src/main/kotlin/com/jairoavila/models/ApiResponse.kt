28c7662996529b370d5c43b5fd6b2ed0fcb8fc53
Jairo Avila <jairo.avila@mercadolibre.com.co>
2022-02-17 12:20:29 -0500

    Create Model Classes

---
+++ src/main/kotlin/com/jairoavila/models/ApiResponse.kt
@@ -0,0 +1,9 @@
+package com.jairoavila.models
+
+data class ApiResponse(
+    val success: Boolean,
+    val message: String? = null,
+    val prevPage: Int? = null,
+    val nextPage: Int? = null,
+    val heroes: List<Hero> = emptyList()
+)
